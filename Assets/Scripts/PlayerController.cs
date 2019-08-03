a291bacedf3daebef68378c8c769afc03b574a47
Unknown <dean3712@gmail.com>
2019-08-02 19:13:12 -0600

    Made very basic move script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public float m_MoveSpeed = 5.0f;
+
+    private Rigidbody m_Rigidbody;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Debug.Log("X: " + x + "Y: " + y);
+
+        Vector3 inputVelocity = new Vector3(x, 0f, y) * Time.deltaTime * m_MoveSpeed;
+
+        m_Rigidbody.velocity = inputVelocity;
+    }
+}
