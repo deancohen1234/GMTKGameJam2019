c026a20ce3c089d0da1addaed2f28c51c5f25d37
Unknown <dean3712@gmail.com>
2019-08-02 19:36:43 -0600

    Added dash to player controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Move Properties")]
     public float m_MoveSpeed = 5.0f;
 
+    [Header("Dash Properties")]
+    public float m_DashSpeed = 15.0f;
+    public float m_DisabledMovementTime = 0.10f; //in seconds
+
     private Rigidbody m_Rigidbody;
+
+    private bool m_CanMove = true;
 
     private void Awake()
     {
@@ -25,10 +32,35 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
-        Debug.Log("X: " + x + "Y: " + y);
+        Move(x, y);
 
-        Vector3 inputVelocity = new Vector3(x, 0f, y) * Time.deltaTime * m_MoveSpeed;
+        if (Input.GetAxis("LTrigger") >= 0.8f)
+        {
+            Vector3 direction = new Vector3(x, 0, y);
+            Dash(direction.normalized);
+        }
+    }
+
+    private void Move(float x, float y)
+    {
+        Vector3 inputVelocity = new Vector3(x, 0f, y) * m_MoveSpeed; //no delta time because physcis is doing that for us
 
         m_Rigidbody.velocity = inputVelocity;
+    }
+
+    private void Dash(Vector3 direction)
+    {
+        StartCoroutine(DisablePlayerMovement(m_DisabledMovementTime));
+
+        m_Rigidbody.velocity = direction * m_DashSpeed;
+    }
+
+    private IEnumerator DisablePlayerMovement(float time)
+    {
+        m_CanMove = false;
+
+        yield return new WaitForSeconds(time);
+
+        m_CanMove = true;
     }
 }
