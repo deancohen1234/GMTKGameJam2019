3087d948e884f7735bb70e704c81371b9c908a3b
Unknown <dean3712@gmail.com>
2019-10-28 19:27:11 -0600

    Fixed bug with crazy knife physics

---
+++ Assets/Scripts/LimitVelocity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class LimitVelocity : MonoBehaviour
+{
+    public float m_MaxMagnitude = 10f;
+
+    private Rigidbody m_RB;
+
+    private void Start()
+    {
+        m_RB = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 velocity = m_RB.velocity;
+        float magnitude = m_RB.velocity.magnitude;
+
+        float clampedMagnitude = Mathf.Clamp(magnitude, 0, m_MaxMagnitude);
+        Vector3 newVelocity = velocity.normalized * clampedMagnitude;
+
+        m_RB.velocity = newVelocity;
+    }
+}
