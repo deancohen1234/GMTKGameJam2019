835eef0bfd0442a4fabad796c940335729916138
Dean <dean3712@gmail.com>
2020-02-29 13:12:38 -0700

    Added framework for totem mechanic

--- Assets/Scripts/LevelMechanics/PestilentialFlood.cs
+++ Assets/Scripts/LevelMechanics/PestilentialFlood.cs
@@ -6,10 +6,15 @@ public class PestilentialFlood : MonoBehaviour
 {
     public LevelMechanicsContainer m_Container;
 
+    public Totem[] m_Totems;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < m_Totems.Length; i++)
+        {
+            m_Totems[i].GetComponent<TriggerCollider>().m_OnTriggerStay += OnTotemStay;
+        }
     }
 
     // Update is called once per frame
@@ -24,7 +29,51 @@ public class PestilentialFlood : MonoBehaviour
 
     void ActivateFlood()
     {
+        Debug.Log("Flood is coming...");
         m_Container.GetPlayerOne().GetHealthComponent().SetDamageMultiplier(2f);
         m_Container.GetPlayerTwo().GetHealthComponent().SetDamageMultiplier(2f);
+    }
+
+    private void OnTotemStay(Collider other, GameObject owner)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+        Totem totem = owner.GetComponent<Totem>();
+
+        if (player != null && totem != null)
+        {
+            if (totem.IsChangeable())
+            {
+                if (player.IsDisarming())
+                {
+                    OnPlayerActivation(player, totem);
+                }
+            }
+
+        }
+    }
+
+    private void OnPlayerActivation(PlayerController player, Totem totem)
+    {
+        totem.SetActivity(true);
+        Debug.Log("Totem Activated");
+
+        if (AreAllTotemsActivated())
+        {
+            ActivateFlood();
+        }
+    }
+
+    private bool AreAllTotemsActivated()
+    {
+        //if any totem is not activated, then return false
+        for (int i = 0; i < m_Totems.Length; i++)
+        {
+            if (m_Totems[i].IsActivated() == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
---
+++ Assets/Scripts/LevelMechanics/Totem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Totem : MonoBehaviour
+{
+    public float m_CooldownTime = 3f; //in seconds
+
+    private float m_LastChangeTime = 0.0f;
+    private bool m_IsActivated;
+
+    public bool IsChangeable()
+    {
+        if (Time.time - m_LastChangeTime >= m_CooldownTime)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void SetActivity(bool isActive)
+    {
+        m_IsActivated = isActive;
+        m_LastChangeTime = Time.time;
+    }
+
+    public bool IsActivated()
+    {
+        return m_IsActivated;
+    }
+}
